a18277368a67fff67d4e95c0462aa22aca6a2363
kevinyoo <jyoo18@calpoly.edu>
2020-03-10 14:40:31 -0700

    Adding classes to git

---
+++ src/main/scala/Q2.scala
@@ -0,0 +1,12 @@
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.{SparkConf, SparkContext}
+
+object Q2 {
+  def main(args: Array[String]) = {
+    Logger.getLogger("org").setLevel(Level.OFF)
+    Logger.getLogger("akka").setLevel(Level.OFF)
+
+    val conf = new SparkConf().setAppName("NameOfApp").setMaster("local[4]")
+    val sc = new SparkContext(conf)
+  }
+}
